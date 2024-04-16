b57ae9b7b7f55b678bd374c1862c8a205b0764ce
Nicele Reyes <nicele.reyes26@gmail.com>
2024-04-16 21:40:08 +0800

    update project 1 > shopping cart react

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    const main = document.querySelector(".main");
 
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-    if (menu.classList.contains("open")){
-        main.classList.add("hide");
-    }else{
-        main.classList.remove("hide");
-    }
 }
